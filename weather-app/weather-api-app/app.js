f22f89746d62dcd89f570ae3c0deb235f8da52c7
Sumer7121 <143977296+Sumer7121@users.noreply.github.com>
2023-10-16 02:00:20 +0000

    weather update

---
+++ weather-app/weather-api-app/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Define the /api/v1/weather route
+app.get('/api/v1/weather', (req, res) => {
+  const weatherData = {
+    Temp: 90.5,
+    Unit: 'fahrenheit',
+    Cloudy: false,
+    PrecipitationChance: 0
+  };
+
+  res.json(weatherData);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Use the route "http://localhost:3000/api/v1/weather" to open the app
